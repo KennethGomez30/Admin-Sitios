6ec51a84c17944658194f4439cb12df24a4e03de
ismaelsolanomorera2004-a11y <ismaelsolanomorera2004@gmail.com>
2026-02-05 10:32:40 -0600

    Add Asientos page and update DB connection
    Add a new Razor Page 'Asientos' (Asientos.cshtml) and its PageModel (Asientos.cshtml.cs). Update appsettings.json DefaultConnection to use the remote MySQL server (tiusr12pl.cuc-carrera-ti.ac.cr) and switch the database/credentials to the target environment.

---
+++ Sistema_Contable/Pages/Asientos/Asientos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistema_Contable.Pages
+{
+    public class AsientosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
